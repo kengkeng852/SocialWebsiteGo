5c46da72403cf3bbc76ab9f29dc3d034a56cafa5
Kongphob Wutthiumphol <kongphobkong852@hotmail.com>
2024-12-19 18:47:34 +0700

    create db

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/joho/godotenv"
 
+	"gihub.com/kengkeng852/SocialWebsiteGo/internal/db"
 	"gihub.com/kengkeng852/SocialWebsiteGo/internal/env"
 	"gihub.com/kengkeng852/SocialWebsiteGo/internal/store"
 )
@@ -16,13 +17,32 @@ func main() {
 
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/socialwebsitego?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+	log.Print("database connection pool established")
+
+	store := store.NewStorage(db)
 
 	app := application{
 		config: cfg,
-		store: store,
+		store:  store,
 	}
 
 	mux := app.mount()
